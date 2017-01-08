96ee94cd190eb65fcda09d3ae0acfb73611ba2ee
Marina Toledo <marinatoledo@Marinas-MacBook-Air.local>
2017-01-08 01:42:53 -0200

    adicao de menu, mudancas dos modais, adicao de media query, etc

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// alert("Arrumar o tamanho das imagens, link para os projetos");
+
+// /*
+// * Open the drawer when the menu ison is clicked.
+// */
+// var menu = document.querySelector('#menu');
+// var main = document.querySelector('main');
+// var drawer = document.querySelector('.nav');
+
+// menu.addEventListener('click', function(e) {
+// drawer.classList.toggle('open');
+// e.stopPropagation();
+// });
+
+// main.addEventListener('click', function() {
+// drawer.classList.remove('open');
+// });
